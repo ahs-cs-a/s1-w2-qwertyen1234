8d7ba32989e4f2a7de34de776183f04ff9999d0d
qwertyen1234 <andrewtw12345@gmail.com>
2024-08-23 16:18:12 +0000

    A

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,49 @@
+public class BodyOfWater{
+    private String name;
+    private double largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
+    
+
+    public BodyOfWater(String Name, double ldiam, double avgdep, boolean saltwtr){
+        name = Name;
+        largestDiameter = ldiam;
+        avgDepth = avgdep;
+        isSaltWater = saltwtr;
+
+    }
+
+    public BodyOfWater(String n, double avgdep){
+        name = n;
+        avgDepth = avgdep;
+
+    }
+
+
+    public String getName(){
+        return name;
+    }
+    
+    public double getlargestDiameter(){
+        return 
+    }
+
+    public double getavgDepth(){
+        return avgDepth;
+    }
+
+    public boolean getisSaltWater(){
+        return isSaltWater;
+
+}
+
+
+
+
+
+
+
+
+
+}
 
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,14 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
+        BodyOfWater b = new BodyOfWater("Lake Michigan" , 118, 279, false);
+        BodyOfWater b1 = new BodyOfWater("Lake Erie" , 57, 62, false);
+
+        b.setName("Atlantic Ocean");
+        b.setlargestDiameter(1770);
+        b.setavgDepth(10932);
+        b.setisSaltWater(false);
+
+
         System.out.println("here");
     }
 }
